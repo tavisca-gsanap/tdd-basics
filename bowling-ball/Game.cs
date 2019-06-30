54412b4e7346602e10fec5072f3bd361bf92beb9
Govind Sanap <gsanap@tavisca.in>
2019-06-30 16:01:04 +0530

    Written program and Tests

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,16 +4,58 @@ namespace BowlingBall
 {
     public class Game
     {
+        private int[] rolls = new int[21];
+        private int[] frame = new int[10];
+        int currentRoll = 0;
+        private bool isSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        private bool isStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            rolls[currentRoll++] = pins;
+        }
+
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                rolls[currentRoll++] = pins[i];
+            }
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
-        }
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else if (isStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
 
+            }
+
+            return score;
+        }
     }
 }
 
